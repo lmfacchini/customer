5d92416ec2c7db56a7d5f49ed09b9b8042a2a30b
Leonardo Marquini Facchini <lmfacchini@gmail.com>
2021-06-14 20:23:49 -0300

    ok

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-From jetty:11.0.4-jre11
-RUN rm -rf /var/lib/jetty/webapps/*
-COPY ./customer-ms-ws/target/customer_v1.war /var/lib/jetty/webapps/customer_v1.war
+From tomcat:9-jdk11-openjdk
+RUN rm -rf /usr/local/tomcat/webapps/*
+COPY ./customer-ms-ws/target/customer_v1.war /usr/local/tomcat/webapps/customer_v1.war
 RUN $JETTY_HOME/bin/jetty.sh start
--- customer-ms-ws/src/main/java/com/builders/customer/ws/app/CustomerApplication.java
+++ customer-ms-ws/src/main/java/com/builders/customer/ws/app/CustomerApplication.java
@@ -3,16 +3,23 @@ package com.builders.customer.ws.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @ComponentScan("com.builders.customer")
 @SpringBootApplication
-public class CustomerApplication {
+public class CustomerApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CustomerApplication.class);
     }
 
 
