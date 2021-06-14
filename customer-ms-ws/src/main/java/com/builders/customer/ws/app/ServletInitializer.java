ca6f6f36109e25561eb9a93d6eb79c59457494c7
Leonardo Marquini Facchini <lmfacchini@gmail.com>
2021-06-14 19:24:41 -0300

    ok

--- customer-ms-ws/src/main/java/com/builders/customer/ws/app/ServletInitializer.java
+++ customer-ms-ws/src/main/java/com/builders/customer/ws/app/ServletInitializer.java
@@ -3,13 +3,19 @@ package com.builders.customer.ws.app;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 
 public class ServletInitializer extends SpringBootServletInitializer   {
 
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
         return application.sources(CustomerApplication.class);
+    }
+
+    public void onStartup(ServletContext servletContext) throws ServletException{
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
     }
 }
