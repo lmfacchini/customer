c533a8d1e8fc5a8c0e66196e1db331027a2e39ec
Leonardo Marquini Facchini <lmfacchini@gmail.com>
2021-06-14 20:58:42 -0300

    ok

--- customer-ms-ws/src/main/java/com/builders/customer/ws/app/CustomerConfiguration.java
+++ customer-ms-ws/src/main/java/com/builders/customer/ws/app/CustomerConfiguration.java
@@ -1,12 +1,10 @@
 package com.builders.customer.ws.app;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories(basePackages="com.builders.customer.core.repository")
-@ComponentScan("com.builders.customer")
 @Configuration
 @EnableAutoConfiguration
 public class CustomerConfiguration {
